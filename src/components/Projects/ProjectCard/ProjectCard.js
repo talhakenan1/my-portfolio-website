4bf321e70c811e7274f4b084b75f15c401ac8531
talhakenan1 <98469908+talhakenan1@users.noreply.github.com>
2025-02-17 23:29:26 +0300

    Add files via upload

---
+++ src/components/Projects/ProjectCard/ProjectCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { ProjectList } from "../../../data/ProjectData";
+import {
+  Card,
+  CardLeft,
+  CardRight,
+  TechCardContainer,
+  TechCard,
+  BtnGroup,
+} from "./ProjectCardElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function ProjectCard() {
+  return (
+    <>
+      {ProjectList.map((list, index) => (
+        <ScrollAnimation animateIn="fadeInLeft" key={index}>
+          <Card>
+            <CardLeft>
+              <img src={list.img} alt={list.name} />
+            </CardLeft>
+            <CardRight>
+              <h4>{list.title}</h4>
+              <p>{list.description}</p>
+              <TechCardContainer>
+                {list.tech_stack.map((tech, index) => (
+                  <TechCard key={index}>{tech}</TechCard>
+                ))}
+              </TechCardContainer>
+              <BtnGroup>
+                {list.github_url.length > 0 && (
+                  <a
+                    className="btn SecondaryBtn btn-shadow"
+                    href={list.github_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Github
+                  </a>
+                )}
+
+              </BtnGroup>
+            </CardRight>
+          </Card>
+        </ScrollAnimation>
+      ))}
+    </>
+  );
+}
+
+export default ProjectCard;
